dc5440e063f16b3c9cc1cb788cd415394f68f6c4
gabrielrosin <gabriel.s.rosin@gmail.com>
2022-07-26 14:44:45 -0300

    Refactors code

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,5 +1,19 @@
 import "./style.css";
 
-export default function Button() {
-    return (<button>Meu botão</button>)
+export default function Button({ type }) {
+    function defineButtonAction(action) {
+        switch (action) {
+            case "home":
+                return <button>Voltar para Home</button>
+            case "reservar assentos":
+                return <button>Reservar assento(s)</button>
+            default:
+                return <button className="time">15:00</button>
+        }
+    }
+
+
+    return (<>
+        {defineButtonAction(type)}
+    </>)
 }
--- src/Movie/Movie.js
+++ src/Movie/Movie.js
@@ -1,3 +1,4 @@
+import Button from "../Button/Button";
 import Footer from "../Footer/Footer";
 import "./style.css";
 import movie from "../assets/image 3.png";
@@ -9,27 +10,32 @@ export default function Movie() {
             <div className="day">
                 <p>Quinta-feira - 24/06/2022</p>
                 <div className="times">
-                    <div className="time">15:00</div>
-                    <div className="time">15:00</div>
-                    <div className="time">15:00</div>
-                    <div className="time">15:00</div>
-                    <div className="time">15:00</div>
-                    <div className="time">15:00</div>
-                    <div className="time">15:00</div>
-                    <div className="time">15:00</div>
-                    <div className="time">15:00</div>
-                    <div className="time">15:00</div>
-                    <div className="time">15:00</div>
-                    <div className="time">15:00</div>
+                    <Button />
+                    <Button />
+                    <Button />
+                    <Button />
+                    <Button />
+                    <Button />
+                    <Button />
+                    <Button />
+                    <Button />
+                    <Button />
+                    <Button />
+                    <Button />
+                    <Button />
+                    <Button />
+                    <Button />
+                    <Button />
+                    <Button />
+                    <Button />
                 </div>
             </div>
             <div className="day">
                 <p>Quinta-feira - 24/06/2022</p>
                 <div className="times">
-                    <div className="time">15:00</div>
-                    <div className="time">15:00</div>
-                    <div className="time">15:00</div>
-                    <div className="time">15:00</div>
+                    <Button />
+                    <Button />
+                    <Button />
                 </div>
             </div>
             <Footer img={movie} />
