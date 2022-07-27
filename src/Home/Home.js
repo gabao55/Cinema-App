a6978d680508d6e36b8cf26352dad139544b9372
gabrielrosin <gabriel.s.rosin@gmail.com>
2022-07-27 07:33:27 -0300

    Receives all movies from API and renders them

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,21 +1,31 @@
 import "./style.css";
-import movie from "../assets/image 3.png";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-export default function Home() {
+export default function Home({ moviesIdList, setMoviesIdList }) {
+    const [moviesList, setMoviesList] = useState([]);
+
+    useEffect(() => {
+        const promise = axios.get("https://mock-api.driven.com.br/api/v5/cineflex/movies");
+
+        promise.then((response) => {
+            setMoviesList(response.data)
+
+            const ids = response.data.map(movie => movie.id);
+            setMoviesIdList([...ids]);
+        });
+    }, []);
+
     return (
         <div>
             <h2>Selecione o filme</h2>
             <div className="movies">
-                <img src={movie} alt="movie banner" />
-                <img src={movie} alt="movie banner" />
-                <img src={movie} alt="movie banner" />
-                <img src={movie} alt="movie banner" />
-                <img src={movie} alt="movie banner" />
-                <img src={movie} alt="movie banner" />
-                <img src={movie} alt="movie banner" />
-                <img src={movie} alt="movie banner" />
-                <img src={movie} alt="movie banner" />
-                <img src={movie} alt="movie banner" />
+                {moviesList.map((movie) => {
+                    return (<Link key={movie.id} to={`/filme/${movie.id}`}>
+                        <img key={movie.id} src={movie.posterURL} alt={movie.title} />
+                    </Link>)
+                })}
             </div>
         </div>
     )
