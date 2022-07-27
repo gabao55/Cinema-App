c33f0589020d2daf516b74a721335b62fd1d2d3c
gabrielrosin <gabriel.s.rosin@gmail.com>
2022-07-27 07:33:46 -0300

    Implements routes to each movie by its ID

--- src/index.js
+++ src/index.js
@@ -6,15 +6,21 @@ import Home from "./Home/Home";
 import Movie from "./Movie/Movie";
 import Session from "./Session/Session";
 import Success from "./Success/Success";
+import { useState } from "react";
 
 function App() {
+    const [selectedMovieId, setSelectedMovieId] = useState("");
+    const [moviesIdList, setMoviesIdList] = useState([]);
+
     return (
         <>
             <h1>CINEFLEX</h1>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/filme" element={<Movie />} />
+                    <Route path="/" element={<Home moviesIdList={moviesIdList} setMoviesIdList={setMoviesIdList} />} />
+                    {moviesIdList.map((id) => {
+                        return <Route key={id} path={`/filme/${id}`} element={<Movie selectedMovieId={selectedMovieId} setSelectedMovieId={setSelectedMovieId} />} />
+                    })}
                     <Route path="/sessao" element={<Session />} />
                     <Route path="/sucesso" element={<Success />} />
                 </Routes>
