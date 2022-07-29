18d00faff69289d9413c23ed2d5931f30115fa60
gabrielrosin <gabriel.s.rosin@gmail.com>
2022-07-29 18:39:58 -0300

    Adds page when user accesses the success page without filling the information

--- src/Success/Success.js
+++ src/Success/Success.js
@@ -3,27 +3,48 @@ import Button from "../Button/Button";
 import { Link } from "react-router-dom";
 
 export default function Success({ movieData, selectedSeatsList, setSelectedSeatsList, customerData }) {
-    // TODO: Create condition to render page only when there are enough data
+    function renderConfirmation() {
+        return (
+            <>
+                <h2 className="success-message">Pedido feito<br /> com sucesso</h2>
+                <div className="purchase-info">
+                    <b>Filme e sessão</b>
+                    <p>{movieData.movie.title}</p>
+                    <p>{movieData.day.date} {movieData.name}</p>
+                </div>
+                <div className="purchase-info">
+                    <b>Ingressos</b>
+                    {selectedSeatsList.map((seat, index) => <p key={index}>Assento {seat}</p>)}
+                </div>
+                <div className="purchase-info">
+                    <b>Comprador</b>
+                    <p>Nome: {customerData.name}</p>
+                    <p>CPF: {customerData.CPF}</p>
+                </div>
+                <Link to="/" style={{ textDecoration: 'none' }} onClick={() => setSelectedSeatsList([])}>
+                    <Button type="large">Voltar para Home</Button>
+                </Link>
+            </>
+        )
+    }
+
+    function renderError() {
+        return (
+            <>
+                <h2>Erro ao carregar confirmação</h2>
+                <Link to="/" style={{ textDecoration: 'none' }} onClick={() => setSelectedSeatsList([])}>
+                    <Button type="large">Voltar para Home</Button>
+                </Link>
+            </>
+        )
+    }
+    
     return (
         <div className="content-container">
-            <h2 className="success-message">Pedido feito<br /> com sucesso</h2>
-            <div className="purchase-info">
-                <b>Filme e sessão</b>
-                <p>{movieData.movie.title}</p>
-                <p>{movieData.day.date} {movieData.name}</p>
-            </div>
-            <div className="purchase-info">
-                <b>Ingressos</b>
-                {selectedSeatsList.map((seat, index) => <p key={index}>Assento {seat}</p>)}
-            </div>
-            <div className="purchase-info">
-                <b>Comprador</b>
-                <p>Nome: {customerData.name}</p>
-                <p>CPF: {customerData.CPF}</p>
-            </div>
-            <Link to="/" style={{ textDecoration: 'none' }} onClick={() => setSelectedSeatsList([])}>
-                <Button type="large">Voltar para Home</Button>
-            </Link>
+            {movieData.length !== 0 ?
+                renderConfirmation() :
+                renderError()
+            }
         </div>
     )
 }
